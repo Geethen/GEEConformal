b049e3a20b5805ff3e87dabb12ceea270e881c1b
Geethen <geethen.singh@gmail.com>
2023-12-05 14:14:51 +0200

    fixed python CP. code to create global+africa map

--- code/conformalClassifier.py
+++ code/conformalClassifier.py
@@ -22,7 +22,7 @@ class conformalFeatureClassifier(object):
             ee.Feature with 'score' property containing nonconformity score
 
         """
-        score = ee.Number(feature.getNumber(self._createClassDictionary().getString(ee.Algorithms.String(feature.get(self.label)))))
+        score = ee.Number(feature.getNumber(self.clsDict.getString(ee.Algorithms.String(feature.get(self.label)))))
         return feature.set('score', ee.Number(score))
     
     # Function 2
@@ -35,8 +35,7 @@ class conformalFeatureClassifier(object):
         # create class index dictionary
         NCLASSES = len(self.bands)-1
         clsIndex = ee.List.sequence(0, NCLASSES).map(lambda value: ee.Algorithms.String(ee.Number(value).int()))
-        clsDict = ee.Dictionary.fromLists(clsIndex, ee.List(self.bands))
-        return clsDict
+        self.clsDict = ee.Dictionary.fromLists(clsIndex, ee.List(self.bands))
 
     # Function 3
     def _computeQLevel(self, nCal: int):
@@ -79,6 +78,9 @@ class conformalFeatureClassifier(object):
         # Get calibration data
         self._calibration_evaluation_split()
         Cal = self.calibration
+
+        # Create class dictionary
+        self._createClassDictionary()
 
         # Compute nonconformaity scores
         scores = ee.FeatureCollection(Cal.map(lambda ft: self._computeScores(ft)))
@@ -161,8 +163,7 @@ class conformalFeatureClassifier(object):
         """
         Compute coverage (based on test set)
         """
-        clsDict = self._createClassDictionary()
-        searchString = ee.String(clsDict.getString(clsDict.keys().getNumber(ft.get('refLabel'))))
+        searchString = ee.String(self.clsDict.getString(ft.getString('refLabel')))
         sets =  ee.List(ft.get('class_name'))
         result = ee.Algorithms.If(
           sets.contains(searchString), # Check if the list contains the string
@@ -186,114 +187,24 @@ class conformalFeatureClassifier(object):
 
         # Evaluate Marginal coverage (based on test set): compute coverage (correct sets/total label pixels/)
         coverage = Sets.map(lambda ft: self._computeCoverage(ft)).aggregate_sum('CorrectSets').divide(nTest)
-        eeprint(coverage)
 
         print('Average set size:', "{:.2f}".format(avgSetSize.getInfo()))
         print('Empirical (marginal) coverage:', "{:.2f}".format(coverage.getInfo()))
               
         return ee.Feature(None, {'version': self.version, 'Empirical Marginal Coverage': coverage, 'Average Prediction Set Size': avgSetSize})
 
-    def predict(self):
+    #  Function 1
+    #  A binary mask is returned for each potential class.
+    def predict(self, image):
         """
         Predicts the conformal classifier model
         """
-        pass
-        
-
-
-class conformalImageClassifier(object):
-    def __init__(self, probImage: ee.Image, calibration: ee.FeatureCollection, test: ee.FeatureCollection, label:str, alpha: float):
-        self.probImage = probImage
-        self.propertyNames = probImage.bandNames()
-        self.alpha = alpha
-        self.calibration = calibration
-        self.test = test
-        self.label = label
-
-    # Inference stage
-    def quantifyImageUncertainty(self):
-        """
-        Quantify uncertainty for specified alpha using calibrated conformalPredictor
-
-        Args:
-            probImage (ee.Number): Quantile level (qLevel) for which to compute uncertainty
-
-        Returns:
-            (ee.Image): A band for binary mask for each class (0: excluded from set, 1: inluded in set)
-                and the length of each pixels' set.
-        """
-        feature = self._computeScores()
-        qHat = ee.Number(feature.get('qHat'))
-        qHatImage = ee.Image.constant(qHat)
-        classNames = self.probImage.bandNames()
-        def setMask(band):
-            return ee.Image(self.probImage.select([band]).gte(qHatImage).set('class',band)).toInt().rename('band')
-        # Compute binary mask for each class
-        # eeprint(classNames.map(setMask))
-        setMasks = ee.ImageCollection(classNames.map(lambda band: setMask(band)))
-        # Compute the length (number of included classes) for each set
+        # create constant image for threshold
+        qHatImage = ee.Image.constant(self.qhat)
+        # get binary mask, 1(included in set)
+        setMasks = ee.ImageCollection(ee.List(self.bands).map(lambda band:
+            ee.Image(image).select([band]).gte(qHatImage).set('class',band).rename('band')))
+        # get length of set per pixel
         setLength = ee.ImageCollection(setMasks).sum().rename('setLength')
-        return ee.Image(setMasks.toBands()).addBands(setLength)
-
-    # Evaluate conformalPredictor
-    def _quantifyTestUncertainty(self):
-        """
-        Obtain the classification sets for the test data partition
-
-        Args:
-            test (ee.FeatureCollection): 
-
-        Returns:
-            ee.FeatureCollection: Contains three additional properties, 
-                1) classSet - The set for the test feature
-                2) cumProb - The sum of all the class set probabilities
-                3) refLabel - The actual label for the test point
-        """
-
-        # Create a function to iterate over the sorted probabilities.
-        def iterateOverProbabilities(feature, previous):
-
-            previous = ee.Feature(previous)
-            # Get the class name and probability of the current feature.
-            className = feature.get('class_name')
-            probability = feature.getNumber('probability')
-            
-            # Get the probability of the previous feature.
-            prevProb = previous.getNumber('probability')
-
-            newSum = ee.Number(ee.Algorithms.If(probability.gte(self.qHat),
-                        prevProb.add(probability),
-                        prevProb))
-            
-            classNames = ee.Algorithms.If(probability.gte(self.qHat),
-                        ee.List([previous.get('class_name')]).add(className).flatten(),
-                        previous.get('class_name'))
-  
-            return ee.Feature(None, {
-                'refLabel': ee.Algorithms.String(feature.get('label')),
-                'class_name': classNames,
-                'probability': newSum
-            })            
-
-        def computeSets(id):
-            filteredCollection = featureCollection.filter(ee.Filter.eq('id', id))
-            #   cumProb serves as the first feature for iterateProbabilities
-            cumProb = ee.Feature(None).set('probability',0).set('class_name',[])
-            result = filteredCollection.iterate(iterateOverProbabilities, cumProb)
-            return result
-
-        
-        # Create a new feature with 4 properties: feature id, label, class and corresponding probability. 
-        # For each class probability, generate a feature
-        featureCollection = ee.FeatureCollection(self.test.map(lambda ft: ee.FeatureCollection(
-            self.propertyNames.map(lambda propertyName: ee.Feature(None,
-                                                                    {'id': ft.id(),
-                                                                    'label': ft.get(self.label),
-                                                                    'class_name': propertyName,
-                                                                    'probability': ee.Number(ft.get(ee.Algorithms.String(propertyName)))}))
-                                        ))).flatten()
-        # Determine the sets for each test feature
-        uid = featureCollection.aggregate_array('id').distinct()
-        sets = ee.FeatureCollection(uid.map(lambda id: computeSets(id)))
+        sets = ee.Image(setMasks.toBands().addBands(setLength)).rename(ee.List(self.bands).add('setLength')).toInt8().updateMask(1)
         return sets
-
